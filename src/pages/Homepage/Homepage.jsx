55d9f02be06d5cfd1d5644243628487f18391ef2
ashik-08 <ashikurrahman.8october@gmail.com>
2023-10-18 20:34:03 +0600

    feat: added newsletter component on homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import Banner from "../../components/Banner/Banner";
 import FollowUs from "../../components/FollowUs/FollowUs";
 import Footer from "../../components/Footer/Footer";
 import NavBar from "../../components/NavBar/NavBar";
+import Newsletter from "../../components/Newsletter/Newsletter";
 
 const Homepage = () => {
     return (
@@ -9,6 +10,7 @@ const Homepage = () => {
             <NavBar></NavBar>
             <Banner></Banner>
             <FollowUs></FollowUs>
+            <Newsletter></Newsletter>
             <Footer></Footer>
         </div>
     );
